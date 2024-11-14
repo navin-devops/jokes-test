de82fc1f522b4cadffde5e8df3e3d0ab1a07ed16
saleemafroz <saeemafroze@gmail.com>
2024-11-14 20:11:32 +0530

    test

--- src/main/java/com/example/JokeController.java
+++ src/main/java/com/example/JokeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JokeController {
 
-    @GetMapping("/joke")
+    @GetMapping("/")
     public String getJoke() {
         return "Why don't skeletons fight each other? They don't have the guts!";
     }
